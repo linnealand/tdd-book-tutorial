8300704a4258085abd501d51259982190febb0e0
Linnea <linneallindstrom@hotmail.com>
2019-06-03 13:45:56 -0400

    First FT specced out in comments, and now uses unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,32 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8080')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8080')
+
+        # Page title and header
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+        # Enter a to-do list item
+
+        # Type into text box
+
+        # Hit enter, page updates, page lists item
+
+        # There is a text box to add another item
+
+        # Page updates again, shows both items in list
+
+        # Site generates a unique URL
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
